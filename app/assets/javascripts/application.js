b6f9b52ab3160b5d3ff99171bc4a3ebac84b0060
Jonathan Momperousse <bklynbest718@gmail.com>
2018-07-12 00:47:54 +0000

    added sidebar and navigation bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,14 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function () {
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+});
